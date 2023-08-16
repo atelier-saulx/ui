e42b03bd76611e5f53539d98c42d4a8f50401625
yves <misterybeer@gmail.com>
2023-08-16 13:35:24 +0200

    WIP previewer

--- src/components/Previewer/index.tsx
+++ src/components/Previewer/index.tsx
@@ -1,8 +1,8 @@
-import React, { ReactNode, FC } from 'react'
+import React, { ReactNode, FC, useState, useEffect } from 'react'
 import { styled } from 'inlines'
 import { renderOrCreateElement } from '../../../src/utils/renderOrCreateElement'
 
-import * as types from '../types'
+import * as typeprops from '../props.json'
 
 type PreviewerProps = {
   component?: ReactNode
@@ -11,14 +11,38 @@ type PreviewerProps = {
 }
 
 export const Previewer: FC<PreviewerProps> = ({ component, propsName }) => {
-  console.log('Previewer -->', component)
-  console.log('propsname -->', propsName)
+  const [propState, setPropState] = useState({})
 
-  console.log('--> name ', component.type.name)
+  console.log('Previewer -->', component)
+  console.log('propsname -->', typeprops.props[propsName])
+
+  const componentProps = typeprops.props[propsName].props
+
+  console.log('componentProps ', componentProps)
+
+  useEffect(() => {
+    const newObj = {}
+
+    let ObjPropKeys = Object.keys(componentProps)
+
+    ObjPropKeys.map((item, idx) =>
+      component.props[item]
+        ? (newObj[item] = component.props[item])
+        : (newObj[item] = null)
+    )
+
+    console.log('🛍', ObjPropKeys)
+    console.log('🧧', newObj)
+
+    setPropState(newObj)
+  }, [])
+
+  // TODO set the props in a state
+  // generate controls based of the props --> with right values/ options
 
   return (
     <styled.div style={{ padding: 20, border: '1px solid green' }}>
-      {renderOrCreateElement(component, { size: 18, color: 'brand' })}
+      {renderOrCreateElement(component, { propState })}
     </styled.div>
   )
 }
