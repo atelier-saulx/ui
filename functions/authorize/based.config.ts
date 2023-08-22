d761a6f15c2456f171226b719879e518a2f5734e
youzi <youri@saulx.com>
2023-08-22 11:17:51 +0200

    make it work

--- functions/authorize/based.config.ts
+++ functions/authorize/based.config.ts
@@ -1,6 +1,9 @@
 import { BasedAuthorizeFunctionConfig } from '@based/functions'
 
 const config: BasedAuthorizeFunctionConfig = {
+  // @ts-ignore
+  name: 'authorize',
   type: 'authorize',
 }
+
 export default config
