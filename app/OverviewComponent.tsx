43a0ff236440c887c13a1e932a0570f4616da84a
Jim de Beer <beerdejim@gmail.com>
2023-08-22 11:24:58 +0200

    fix

--- app/OverviewComponent.tsx
+++ app/OverviewComponent.tsx
@@ -29,11 +29,7 @@ export const Props: FC<{ component: ComponentDef }> = ({ component }) => {
         <Text style={{ minWidth: 200 }} weight="strong">
           {key}
         </Text>
-        <Text style={{ minWidth: 150 }}>
-          {JSON.stringify(prop.type).trim()}
-        </Text>
-        <Text style={{ flexGrow: 1 }}>Description</Text>
-        <Text style={{ minWidth: 150 }}>Default</Text>
+        <Text style={{ flexGrow: 1 }}>{JSON.stringify(prop.type).trim()}</Text>
       </styled.div>
     )
   }
@@ -61,9 +57,7 @@ export const Props: FC<{ component: ComponentDef }> = ({ component }) => {
         <Text style={{ minWidth: 200 }} weight="strong">
           Name
         </Text>
-        <Text style={{ minWidth: 150 }}>Type</Text>
-        <Text style={{ flexGrow: 1 }}>Description</Text>
-        <Text style={{ minWidth: 150 }}>Default</Text>
+        <Text style={{ flexGrow: 1 }}>Type</Text>
       </styled.div>
       {p}
     </styled.div>
