b7c330017736c0f7ba2a280cfc276f6059e94551
yves <misterybeer@gmail.com>
2023-08-29 17:29:16 +0200

    linegraph porting

---
+++ src/components/Linegraph/types.ts
@@ -0,0 +1,42 @@
+import { DateFormat } from '@based/pretty-date'
+import { NumberFormat } from '@based/pretty-number'
+import { ColorActionColors } from '../..'
+
+export type Point = { x: number; y: number }
+
+export type LineDataInput = {
+  data: Point[]
+  fill?: boolean
+  color?: ColorActionColors
+  valueFormat?: NumberFormat
+  minMax?: boolean
+}
+export type LineData = LineDataInput & {
+  points?: Point[]
+  minX?: number
+  maxX?: number
+  minY?: number
+  maxY?: number
+  stepSize?: number
+  maxData?: Point[]
+  maxPoints?: Point[]
+  minData?: Point[]
+  minPoints?: Point[]
+}
+
+export type LineGraphData = {
+  [key: string]: LineData
+}
+
+export type LineGraphDataInput =
+  | Point[]
+  | { [key: string]: LineDataInput | Point[] }
+
+export type LineXGraphFormat =
+  | 'date'
+  | 'number'
+  | 'date-time-human'
+  | NumberFormat
+  | DateFormat
+  | false
+  | null
