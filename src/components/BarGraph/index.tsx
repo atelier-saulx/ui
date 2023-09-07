ab8d60b70d13ca24a131dfaf3c8441dcf7d99b74
yves <misterybeer@gmail.com>
2023-09-07 11:40:28 +0200

    wip verticalbars

--- src/components/BarGraph/index.tsx
+++ src/components/BarGraph/index.tsx
@@ -21,6 +21,41 @@ type BarGraphProps = {
   style?: Style
 }
 
+const HorizontalBar = ({ display, label, value, percentage }) => {
+  return (
+    <styled.div
+      style={{ display: 'flex', marginBottom: 4, alignItems: 'center' }}
+    >
+      <styled.div style={{ width: '100%' }}>
+        <styled.div
+          style={{
+            borderRadius: 3,
+            backgroundColor: color('nonSemanticBackground', 'magenta', 'muted'),
+            padding: '4px 8px',
+            whiteSpace: 'nowrap',
+            width: `${percentage.toFixed()}%`,
+          }}
+        >
+          <Text weight="medium">{label}</Text>
+        </styled.div>
+      </styled.div>
+      <styled.div
+        style={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          marginLeft: 26,
+          whiteSpace: 'nowrap',
+          width: 28,
+        }}
+      >
+        <Text weight="medium">
+          {display === 'values' ? value : percentage.toFixed(1) + '%'}
+        </Text>
+      </styled.div>
+    </styled.div>
+  )
+}
+
 export const BarGraph: FC<BarGraphProps> = ({
   data,
   display,
@@ -34,50 +69,12 @@ export const BarGraph: FC<BarGraphProps> = ({
     percentage: (item.value / totalValue) * 100,
   }))
 
-  const HorizontalBar = ({ label, value, percentage }) => {
-    return (
-      <styled.div
-        style={{ display: 'flex', marginBottom: 4, alignItems: 'center' }}
-      >
-        <styled.div style={{ width: '100%' }}>
-          <styled.div
-            style={{
-              borderRadius: 3,
-              backgroundColor: color(
-                'nonSemanticBackground',
-                'magenta',
-                'muted'
-              ),
-              padding: '4px 8px',
-              whiteSpace: 'nowrap',
-              width: `${percentage.toFixed()}%`,
-            }}
-          >
-            <Text weight="medium">{label}</Text>
-          </styled.div>
-        </styled.div>
-        <styled.div
-          style={{
-            display: 'flex',
-            justifyContent: 'flex-end',
-            marginLeft: 26,
-            whiteSpace: 'nowrap',
-            width: 28,
-          }}
-        >
-          <Text weight="medium">
-            {display === 'values' ? value : percentage.toFixed(1) + '%'}
-          </Text>
-        </styled.div>
-      </styled.div>
-    )
-  }
-
   return (
     <styled.div style={{ width: '100%', ...style }}>
       {data.map((item, idx) => (
         <HorizontalBar
           key={idx}
+          display={display}
           label={item.label}
           value={item.value}
           percentage={item.percentage}
