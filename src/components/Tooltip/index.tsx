66b3ca79fa243e66e3a24f7569f6a2c7f01a4d76
kylemarch513 <kyle.march513@gmail.com>
2023-09-13 17:37:04 +0200

    tooltip variatns

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -3,13 +3,31 @@ import { styled, Style } from 'inlines'
 import { useTooltip } from '../../hooks/useTooltip'
 
 export const TooltipTest = () => {
-  const toolTipTest = useTooltip('Tooltip placeholder', 'bottom')
+  const thingy = [
+    useTooltip('Tooltip placeholder', 'top-left'),
+    useTooltip('Tooltip placeholder', 'top'),
+    useTooltip('Tooltip placeholder', 'top-right'),
+    useTooltip('Tooltip placeholder', 'left'),
+    useTooltip('Tooltip placeholder', 'left'),
+    useTooltip('Tooltip placeholder', 'right'),
+    useTooltip('Tooltip placeholder', 'bottom-left'),
+    useTooltip('Tooltip placeholder', 'bottom'),
+    useTooltip('Tooltip placeholder', 'bottom-right'),
+  ]
 
   return (
-    <>
-      <styled.div {...toolTipTest} style={{ backgroundColor: 'yellow' }}>
-        flip
-      </styled.div>
-    </>
+    <styled.div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: `repeat(3, 1fr)`,
+        gridGap: '20px',
+      }}
+    >
+      {thingy.map((tooltipTest) => (
+        <styled.div {...tooltipTest} style={{ backgroundColor: 'yellow' }}>
+          Flap
+        </styled.div>
+      ))}
+    </styled.div>
   )
 }
