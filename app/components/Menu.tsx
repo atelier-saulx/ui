7f961ee2b89fec160cb2ae08aa18d2d2459d20a9
kylemarch513 <kyle.march513@gmail.com>
2023-09-28 13:10:12 +0200

    modal warning added and component stuff

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Menu, IconHome, IconEmojiSmile } from '../../src'
+import React, { useState } from 'react'
+import { Menu, IconHome, IconEmojiSmile, Badge } from '../../src'
 import props from '../props.json'
 import { ComponentDef } from '../types'
 
@@ -10,39 +10,54 @@ const example: ComponentDef = {
   properties: props.props.MenuProps.props,
   examples: [
     {
-      props: {
-        collapse: true,
-        data: {
-          Label: {
-            flyp: {
-              value: 'yow1',
-              label: 'Label one',
-              // icon: () => <IconHome />,
-            },
-          },
-          title: {
-            flyp: {
-              value: 'yow1',
-              label: 'Menu Label',
-              // icon: () => <IconEmojiSmile />,
-            },
-            flip: {
-              label: 'Menu Label',
-              // icon: () => <IconHome />,
-            },
-            flop: {
-              value: 'yow3',
-              label: 'Menu Label',
-              // icon: () => <IconHome />,
-            },
-            test: {
-              value: 'x',
-              label: 'more 0',
-              items: [{ value: 'yow4', label: 'flipe' }],
-            },
-            flap: 'Menu Text',
-          },
-        },
+      props: {},
+      customRenderer: ({}) => {
+        const [value, setValue] = useState('yowza1')
+
+        return (
+          <div style={{ display: 'flex', gap: 20 }}>
+            <Badge>{value}</Badge>
+            <Menu
+              onChange={(v) => {
+                setValue(v)
+              }}
+              active={value}
+              data={{
+                Label: {
+                  flyp: {
+                    //@ts-ignore
+                    value: 'yowza1',
+                    label: 'Label one',
+                    // icon: () => <IconHome />,
+                  },
+                },
+                title: {
+                  flyp: {
+                    value: 'yow1',
+                    label: 'Menu Label1',
+                    // icon: () => <IconEmojiSmile />,
+                  },
+                  flip: {
+                    value: 'yow2',
+                    label: 'Menu Label2',
+                    // icon: () => <IconHome />,
+                  },
+                  flop: {
+                    value: 'yow3',
+                    label: 'Menu Label3',
+                    // icon: () => <IconHome />,
+                  },
+                  test: {
+                    value: 'x',
+                    label: 'more 0',
+                    items: [{ value: 'yow4', label: 'flipe' }],
+                  },
+                  flap: 'Menu Text',
+                },
+              }}
+            />
+          </div>
+        )
       },
     },
   ],
--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import props from '../props.json'
 import { ComponentDef } from '../types'
-import { Button, Input, Modal } from '../../src'
+import { Button, Input, Modal, Tab, Tabs, Text } from '../../src'
+import { CheckboxInput } from '../../src/components/Input/CheckboxInput'
 
 const example: ComponentDef = {
   name: 'Modal',
@@ -10,8 +11,11 @@ const example: ComponentDef = {
   component: Modal.Root,
   examples: [
     {
+      description: 'Change Modal',
       props: {},
       customRenderer: () => {
+        const [value, setValue] = useState('Apex')
+
         return (
           <Modal.Root>
             <Modal.Trigger>
@@ -26,9 +30,120 @@ const example: ComponentDef = {
                     <Input
                       type="text"
                       label="Name of company"
-                      value="Apex"
-                      onChange={() => {}}
+                      value={value}
+                      onChange={(v) => {
+                        setValue(v)
+                      }}
                     />
+                  </Modal.Body>
+                  <Modal.Actions>
+                    <Button onClick={close} color="system">
+                      Cancel
+                    </Button>
+                    <Button onClick={close} color="primary">
+                      Save
+                    </Button>
+                  </Modal.Actions>
+                </>
+              )}
+            </Modal.Content>
+          </Modal.Root>
+        )
+      },
+    },
+    {
+      name: 'Alert Modal Example',
+      description: 'Modal Alert',
+      props: {},
+      customRenderer: () => {
+        return (
+          <Modal.Root>
+            <Modal.Trigger>
+              <Button>Open modal</Button>
+            </Modal.Trigger>
+            <Modal.Content>
+              {({ close }) => (
+                <>
+                  <Modal.Title>Delete field</Modal.Title>
+                  <Modal.Body>
+                    <Text>
+                      Are you sure you want to delete the field{' '}
+                      <b>Description</b>
+                    </Text>
+                    <Modal.Warning>
+                      You are about to update the default view <b>Sequence</b>{' '}
+                      for all users.
+                    </Modal.Warning>
+                  </Modal.Body>
+                  <Modal.Actions>
+                    <Button onClick={close} color="system">
+                      Cancel
+                    </Button>
+                    <Button onClick={close} color="primary">
+                      Save
+                    </Button>
+                  </Modal.Actions>
+                </>
+              )}
+            </Modal.Content>
+          </Modal.Root>
+        )
+      },
+    },
+    {
+      name: 'Add custom view',
+      description: 'Another example of this',
+      props: {},
+      customRenderer: () => {
+        return (
+          <Modal.Root>
+            <Modal.Trigger>
+              <Button>Open modal</Button>
+            </Modal.Trigger>
+            <Modal.Content>
+              {({ close }) => (
+                <>
+                  <Modal.Title>Delete field</Modal.Title>
+                  <Modal.Description>
+                    This is your organisation's name within Based. <br />
+                    For example, you can use the name of your company or
+                    department.
+                  </Modal.Description>
+                  <Modal.Body>
+                    <Tabs>
+                      <Tab
+                        label="General"
+                        style={{
+                          display: 'flex',
+                          flexDirection: 'column',
+                          gap: 24,
+                        }}
+                      >
+                        <Modal.Warning type="alert">
+                          You are about to update the default view{' '}
+                          <b>Sequence</b> for all users.
+                        </Modal.Warning>
+                        <span>
+                          <Text style={{}}>Name</Text>
+                          <Text light>
+                            Name that will be displayed in the interace
+                          </Text>
+                        </span>
+                        <CheckboxInput
+                          title="Name"
+                          description="Name that will be displayed in the interface"
+                          value={false}
+                          onChange={() => null}
+                        />
+                        <CheckboxInput
+                          title="Name"
+                          description="Name that will be displayed in the interface"
+                          value={false}
+                          onChange={() => null}
+                        />
+                      </Tab>
+                      <Tab label="Settings">Avatar</Tab>
+                    </Tabs>
                   </Modal.Body>
                   <Modal.Actions>
                     <Button onClick={close} color="system">
