05e21d465a567e22fd424ff79210feba7294ed79
Jim de Beer <beerdejim@gmail.com>
2023-10-02 16:44:20 +0200

    fix header table - remove sticky scince it messes up rendering

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -66,206 +66,198 @@ export function Table({
   )
 
   return (
-    <ScrollArea
-      ref={tableContainerRef}
+    <styled.div
       style={{
-        overflow: 'auto',
         height: '100%',
         width: 'fit-content',
       }}
-      onScroll={(e) => handleScroll(e.target as HTMLDivElement)}
     >
-      <table
+      <styled.div
         style={{
-          width: table.getCenterTotalSize(),
-          // borderCollapse: 'separate',
-          tableLayout: 'fixed',
+          borderTop: `1px solid ${color('border', 'default', 'strong')}`,
+          borderBottom: `1px solid ${color('border', 'default', 'strong')}`,
+          position: 'sticky',
+          top: 0,
+          margin: 0,
+          overflowX: 'hidden',
+          zIndex: 1,
+          transform: 'translate3d(0,0,0)',
+          left: 0,
           background: color('background', 'default', 'strong'),
-          borderSpacing: 0,
-          borderCollapse: 'collapse',
-          userSelect: 'none',
-          WebkitUserSelect: 'none',
         }}
       >
-        <thead
-          style={{
-            position: 'sticky',
-            top: 0,
-            margin: 0,
-            textAlign: 'left',
-            zIndex: 50,
-            background: color('background', 'default', 'strong'),
-          }}
-        >
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header, i) => {
-                return (
-                  <styled.th
-                    // ref={(ref) => (headersRef.current[i] = ref)}
-                    onMouseDown={(e) => {
-                      // onMouseDownHandler(e, i)
-                    }}
-                    key={header.id}
-                    colSpan={header.colSpan}
-                    style={{
-                      padding: '0 12px',
-                      height: 42,
-                      fontSize: 14,
-                      position: 'relative',
-                      overflow: 'hidden',
-                      width: header.getSize(),
-                      boxSizing: 'border-box',
-                      borderLeft: '1px solid transparent',
-                      borderRight: '1px solid transparent',
-                      borderTop: `1px solid ${color(
-                        'border',
-                        'default',
-                        'strong'
-                      )}`,
-                      borderBottom: `1px solid ${color(
-                        'border',
-                        'default',
-                        'strong'
-                      )}`,
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header, i) => {
+              return (
+                <styled.th
+                  key={header.id}
+                  colSpan={header.colSpan}
+                  style={{
+                    padding: '0 12px',
+                    height: 42,
+                    fontSize: 14,
+                    position: 'relative',
+                    overflow: 'hidden',
+                    width: header.getSize(),
+                    boxSizing: 'border-box',
+                    borderLeft: '1px solid transparent',
+                    borderRight: '1px solid transparent',
+
+                    color: header.column.getIsSorted()
+                      ? color('content', 'brand', 'primary')
+                      : color('content', 'default', 'secondary'),
+                    '&:hover': {
                       color: header.column.getIsSorted()
                         ? color('content', 'brand', 'primary')
-                        : color('content', 'default', 'secondary'),
-                      '&:hover': {
-                        color: header.column.getIsSorted()
-                          ? color('content', 'brand', 'primary')
-                          : color('content', 'default', 'primary'),
-                        '& span': {
-                          backgroundColor: color('inputBorder', 'neutralHover'),
-                        },
+                        : color('content', 'default', 'primary'),
+                      '& span': {
+                        backgroundColor: color('inputBorder', 'neutralHover'),
                       },
-                      '&:active': {
-                        '& span': {
-                          backgroundColor: color('inputBorder', 'active'),
-                        },
+                    },
+                    '&:active': {
+                      '& span': {
+                        backgroundColor: color('inputBorder', 'active'),
                       },
-                    }}
-                  >
-                    {header.isPlaceholder ? null : (
-                      <Text
-                        onClick={header.column.getToggleSortingHandler()}
-                        style={{
-                          display: 'flex',
-                          height: '100%',
-                          alignItems: 'center',
-                          userSelect: 'none',
-                          cursor: 'pointer',
-                          color: 'inherit',
-                        }}
-                      >
-                        {{
-                          asc: (
-                            <IconSort
-                              style={{
-                                marginRight: 8,
-                                transform: 'scaleY(-1)',
-                              }}
-                              color="inherit"
-                            />
-                          ),
-                          desc: (
-                            <IconSort
-                              style={{ marginRight: 8 }}
-                              color="inherit"
-                            />
-                          ),
-                        }[header.column.getIsSorted() as string] ?? null}
-                        {flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                      </Text>
-                    )}
-                    <styled.span
-                      // tabIndex={1}
-                      onMouseDown={header.getResizeHandler()}
-                      onTouchStart={header.getResizeHandler()}
-                      className={`resizer ${
-                        header.column.getIsResizing() ? 'isResizing' : ''
-                      }`}
+                    },
+                  }}
+                >
+                  {header.isPlaceholder ? null : (
+                    <Text
+                      onClick={header.column.getToggleSortingHandler()}
                       style={{
-                        position: 'absolute',
-                        right: '0',
-                        top: '0',
+                        display: 'flex',
                         height: '100%',
-                        width: '2px',
-                        // background: 'rgba(0, 0, 0, 0.5)',
-                        cursor: 'col-resize',
+                        alignItems: 'center',
                         userSelect: 'none',
-                        touchAction: 'none',
-                        // '&:hover': {
-                        //   // Input Border/Neutral Normal
-                        //   backgroundColor: color('inputBorder', 'neutralHover'),
-                        // },
-
-                        transform:
-                          resizeMode === 'snap' && header.column.getIsResizing()
-                            ? `translateX(${
-                                table.getState().columnSizingInfo.deltaOffset
-                              }px)`
-                            : '',
+                        cursor: 'pointer',
+                        color: 'inherit',
                       }}
-                    />
-                  </styled.th>
-                )
-              })}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {paddingTop > 0 && (
-            <tr>
-              <td style={{ height: `${paddingTop}px` }} />
-            </tr>
-          )}
-          {virtualRows.map((virtualRow) => {
-            const row = rows[virtualRow.index]
-            return (
-              <tr key={row.id}>
-                {row.getVisibleCells().map((cell) => {
-                  return (
-                    <td
-                      style={{
-                        boxSizing: 'border-box',
-                        height: 61,
-                        padding: '0 12px',
-                        borderBottom: `1px solid ${color(
-                          'border',
-                          'default',
-                          'strong'
-                        )}`,
-                        overflow: 'hidden',
-                        textOverflow: 'ellipsis',
-                        whiteSpace: 'nowrap',
-                        maxWidth: '100%',
-                        width: cell.column.getSize(),
-                      }}
-                      key={cell.id}
                     >
-                      <Text>
-                        {flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext()
-                        )}
-                      </Text>
-                    </td>
-                  )
-                })}
+                      {{
+                        asc: (
+                          <IconSort
+                            style={{
+                              marginRight: 8,
+                              transform: 'scaleY(-1)',
+                            }}
+                            color="inherit"
+                          />
+                        ),
+                        desc: (
+                          <IconSort
+                            style={{ marginRight: 8 }}
+                            color="inherit"
+                          />
+                        ),
+                      }[header.column.getIsSorted() as string] ?? null}
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                    </Text>
+                  )}
+                  <styled.span
+                    onMouseDown={header.getResizeHandler()}
+                    onTouchStart={header.getResizeHandler()}
+                    className={`resizer ${
+                      header.column.getIsResizing() ? 'isResizing' : ''
+                    }`}
+                    style={{
+                      position: 'absolute',
+                      right: '0',
+                      top: '0',
+                      height: '100%',
+                      width: '2px',
+                      cursor: 'col-resize',
+                      userSelect: 'none',
+                      touchAction: 'none',
+                      transform:
+                        resizeMode === 'snap' && header.column.getIsResizing()
+                          ? `translateX(${
+                              table.getState().columnSizingInfo.deltaOffset
+                            }px)`
+                          : '',
+                    }}
+                  />
+                </styled.th>
+              )
+            })}
+          </tr>
+        ))}
+      </styled.div>
+
+      <ScrollArea
+        ref={tableContainerRef}
+        style={{
+          overflow: 'auto',
+          height: 'calc(100% - 42px)',
+          width: 'fit-content',
+        }}
+        onScroll={(e) => handleScroll(e.target as HTMLDivElement)}
+      >
+        <table
+          style={{
+            width: table.getCenterTotalSize(),
+            tableLayout: 'fixed',
+            position: 'relative',
+            background: color('background', 'default', 'strong'),
+            borderSpacing: 0,
+            borderCollapse: 'collapse',
+            userSelect: 'none',
+            WebkitUserSelect: 'none',
+          }}
+        >
+          <tbody>
+            {paddingTop > 0 && (
+              <tr>
+                <td style={{ height: `${paddingTop}px` }} />
               </tr>
-            )
-          })}
-          {paddingBottom > 0 && (
-            <tr>
-              <td style={{ height: `${paddingBottom}px` }} />
-            </tr>
-          )}
-        </tbody>
-      </table>
-    </ScrollArea>
+            )}
+            {virtualRows.map((virtualRow) => {
+              const row = rows[virtualRow.index]
+              return (
+                <tr key={row.id}>
+                  {row.getVisibleCells().map((cell) => {
+                    return (
+                      <td
+                        style={{
+                          boxSizing: 'border-box',
+                          height: 61,
+                          padding: '0 12px',
+                          borderBottom: `1px solid ${color(
+                            'border',
+                            'default',
+                            'strong'
+                          )}`,
+                          overflow: 'hidden',
+                          textOverflow: 'ellipsis',
+                          whiteSpace: 'nowrap',
+                          maxWidth: '100%',
+                          width: cell.column.getSize(),
+                        }}
+                        key={cell.id}
+                      >
+                        <Text>
+                          {flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext()
+                          )}
+                        </Text>
+                      </td>
+                    )
+                  })}
+                </tr>
+              )
+            })}
+            {paddingBottom > 0 && (
+              <tr>
+                <td style={{ height: `${paddingBottom}px` }} />
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </ScrollArea>
+    </styled.div>
   )
 }
