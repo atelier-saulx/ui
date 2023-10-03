94347cf7d164e47035fcd2655942eea409e56e08
Jim de Beer <beerdejim@gmail.com>
2023-10-03 15:06:32 +0200

    add todo

--- based.schema.ts
+++ based.schema.ts
@@ -7,6 +7,36 @@ export default {
     user: {
       prefix: 'us',
     },
+    todo: {
+      meta: {
+        pluralName: '',
+        description: 'We have many things todo!',
+        name: 'Todo',
+      },
+      fields: {
+        done: {
+          meta: {
+            name: 'done',
+            index: 6,
+          },
+          type: 'boolean',
+        },
+        name: {
+          meta: {
+            name: 'name',
+            index: 4,
+          },
+          type: 'string',
+        },
+        cycle: {
+          meta: {
+            name: 'cycle',
+            index: 5,
+          },
+          type: 'int',
+        },
+      },
+    },
     theme: {
       prefix: 'th',
       fields: {
