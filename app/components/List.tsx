f9c0db88cb1f361b6888f79638e6d4375724bd0a
Jim de Beer <beerdejim@gmail.com>
2023-10-04 12:35:06 +0200

    fix

---
+++ app/components/List.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { Pill } from '../../src'
+import { ComponentDef } from '../types'
+import { faker } from '@faker-js/faker'
+
+const example: ComponentDef = {
+  name: 'List',
+  component: Pill,
+  description: 'Pill ',
+  properties: {},
+  examples: [
+    {
+      props: {},
+      customRenderer: () => {
+        const [value, setValue] = useState<string>('')
+        const [options] = useState(() =>
+          Array.from({ length: 25 }).map((_, i) => ({
+            label: faker.person.fullName(),
+            value: `id${i}`,
+          }))
+        )
+
+        return (
+          <Pill
+            prefix="lala"
+            value={value}
+            onChange={(v) => {
+              setValue(v)
+            }}
+            options={options}
+            placeholder="Select one"
+          />
+        )
+      },
+    },
+  ],
+}
+
+export default example
