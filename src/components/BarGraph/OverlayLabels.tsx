ee43dc27c03b4eb2e35e9147280e14a051fb0fc0
yves <misterybeer@gmail.com>
2023-11-06 11:13:25 +0100

    overlay labels bargraph

--- src/components/BarGraph/OverlayLabels.tsx
+++ src/components/BarGraph/OverlayLabels.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Text } from '../Text'
 import { styled } from 'inlines'
 import { color, colorHash } from '../../varsUtilities'
 import { NumberFormat, prettyNumber } from '@based/pretty-number'
+import { useWindowResize } from '../../hooks'
 
 export const OverlayLabels = ({
   xPos,
@@ -13,17 +14,30 @@ export const OverlayLabels = ({
   direction,
   label,
 }) => {
+  const { width, height } = useWindowResize()
+  const labelRef = useRef<HTMLDivElement>()
+  const labelWidth = labelRef?.current?.clientWidth
+  const labelHeight = labelRef?.current?.clientHeight
+
   return (
     <styled.div
+      ref={labelRef}
       style={{
         backgroundColor: color('background', 'default', 'muted'),
-
         borderRadius: 4,
         position: 'absolute',
         zIndex: 1,
         left: xPos,
         top: yPos,
         border: `1px solid ${color('inputBorder', 'neutralNormal')}`,
+        transform:
+          yPos + labelHeight > height - 16 && xPos + labelWidth > width - 16
+            ? 'translateY(-100%) translateX(-110%)'
+            : yPos + labelHeight > height - 16
+            ? 'translateY(-100%) translateX(0%)'
+            : xPos + labelWidth > width - 16
+            ? 'translateY(0%) translateX(-100%)'
+            : 'translateY(0%) translateX(0%)',
       }}
     >
       {label && (
