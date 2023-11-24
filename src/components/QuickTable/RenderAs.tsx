c1ee129cd936eb34d43e0e77fc6b24b06a01f665
yves <misterybeer@gmail.com>
2023-11-24 16:22:35 +0100

    s

--- src/components/QuickTable/RenderAs.tsx
+++ src/components/QuickTable/RenderAs.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import { Text } from '../Text'
 import { Badge } from '../Badge'
+import { prettyDate } from '@based/pretty-date'
+import { prettyNumber } from '@based/pretty-number'
+import { styled } from 'inlines'
+import { Thumbnail } from '../Thumbnail'
+import { IconAttachment } from 'src/icons'
 
 export const RenderAs = ({ colName, input }) => {
   let cName = colName.toLowerCase()
 
   if (cName === 'id') {
     return <Badge light>{input}</Badge>
+  } else if (cName === 'createdat' || cName === 'updatedat') {
+    return <Text>{prettyDate(input, 'date-time-human')}</Text>
+  } else if (cName === 'size') {
+    return <Text>{prettyNumber(input, 'number-bytes')}</Text>
+  } else if (cName === 'src' || cName === 'thumb') {
+    return (
+      <Thumbnail src={input} size="small" icon={<IconAttachment />} light />
+    )
   }
 
-  return <Text>{input}</Text>
+  return <Text truncate>{input}</Text>
 }
