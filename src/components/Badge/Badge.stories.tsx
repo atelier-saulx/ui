ec8d3a504cd277d395058f4978d81f010aeb37ab
Vass Bence <49574140+vassbence@users.noreply.github.com>
2024-07-16 16:17:33 +0200

    add new badges

--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -5,37 +5,82 @@ export default {
   component: Badge,
 }
 
-export const Neutral = {
+export const NeutralFilled = {
   args: {
     children: 'Published',
     color: 'neutral',
+    type: 'filled',
   },
 }
 
-export const Red = {
+export const RedFilled = {
   args: {
     children: 'Published',
     color: 'red',
+    type: 'filled',
   },
 }
 
-export const Indigo = {
+export const IndigoFilled = {
   args: {
     children: 'Published',
     color: 'indigo',
+    type: 'filled',
   },
 }
 
-export const Green = {
+export const GreenFilled = {
   args: {
     children: 'Published',
     color: 'green',
+    type: 'filled',
   },
 }
 
-export const Amber = {
+export const AmberFilled = {
   args: {
     children: 'Published',
     color: 'amber',
+    type: 'filled',
+  },
+}
+
+export const NeutralSubtle = {
+  args: {
+    children: 'Published',
+    color: 'neutral',
+    type: 'subtle',
+  },
+}
+
+export const RedSubtle = {
+  args: {
+    children: 'Published',
+    color: 'red',
+    type: 'subtle',
+  },
+}
+
+export const IndigoSubtle = {
+  args: {
+    children: 'Published',
+    color: 'indigo',
+    type: 'subtle',
+  },
+}
+
+export const GreenSubtle = {
+  args: {
+    children: 'Published',
+    color: 'green',
+    type: 'subtle',
+  },
+}
+
+export const AmberSubtle = {
+  args: {
+    children: 'Published',
+    color: 'amber',
+    type: 'subtle',
   },
 }
--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -5,34 +5,59 @@ import { Text } from '../Text/index.js'
 type BadgeProps = {
   children: string
   color?: 'neutral' | 'red' | 'green' | 'indigo' | 'amber'
+  type?: 'filled' | 'subtle'
 }
 
-function Badge({ children, color = 'neutral' }: BadgeProps) {
+function Badge({ children, color = 'neutral', type = 'filled' }: BadgeProps) {
   return (
     <div
       style={{
         display: 'inline-flex',
         padding: '4px 6px',
         borderRadius: radius[4],
-        ...(color === 'neutral' && {
-          background: colors.neutral20,
-          color: colors.neutral100,
+        ...(type === 'filled' && {
+          ...(color === 'neutral' && {
+            background: colors.neutral100,
+            color: colors.neutralInverted100,
+          }),
+          ...(color === 'red' && {
+            background: colors.red100,
+            color: colors.white100,
+          }),
+          ...(color === 'green' && {
+            background: colors.green100,
+            color: colors.white100,
+          }),
+          ...(color === 'indigo' && {
+            background: colors.indigo100,
+            color: colors.white100,
+          }),
+          ...(color === 'amber' && {
+            background: colors.amber100,
+            color: colors.white100,
+          }),
         }),
-        ...(color === 'red' && {
-          background: colors.red20,
-          color: colors.red100,
-        }),
-        ...(color === 'green' && {
-          background: colors.green20,
-          color: colors.green100,
-        }),
-        ...(color === 'indigo' && {
-          background: colors.indigo20,
-          color: colors.indigo100,
-        }),
-        ...(color === 'amber' && {
-          background: colors.amber20,
-          color: colors.amber100,
+        ...(type === 'subtle' && {
+          ...(color === 'neutral' && {
+            background: colors.neutral20,
+            color: colors.neutral100,
+          }),
+          ...(color === 'red' && {
+            background: colors.red20,
+            color: colors.red100,
+          }),
+          ...(color === 'green' && {
+            background: colors.green20,
+            color: colors.green100,
+          }),
+          ...(color === 'indigo' && {
+            background: colors.indigo20,
+            color: colors.indigo100,
+          }),
+          ...(color === 'amber' && {
+            background: colors.amber20,
+            color: colors.amber100,
+          }),
         }),
       }}
     >
