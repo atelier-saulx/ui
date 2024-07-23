534da5ff648bbb264fb94b86efb3a85af42285df
Vass Bence <49574140+vassbence@users.noreply.github.com>
2024-07-23 21:05:46 +0200

    cleanup repo

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   publicDir: './src/assets',
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts'],
-  },
 })
