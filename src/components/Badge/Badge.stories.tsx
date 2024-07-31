75e19800d333d42456cf27653a4e8959913bab24
Vass Bence <49574140+vassbence@users.noreply.github.com>
2024-07-31 12:21:40 +0200

    rework badge

--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Badge } from './index.js'
 
 export default {
@@ -6,100 +7,25 @@ export default {
 }
 
 export const Default = {
-  args: {
-    children: 'Going up +20%',
-    color: 'green',
-    type: 'filled',
-    leadIcon: 'arrow-up',
-    trailIcon: 'arrow-up',
-  },
-}
-
-export const SingleIcon = {
-  args: {
-    children: 'Going up +20%',
-    color: 'green',
-    type: 'filled',
-    leadIcon: 'arrow-up',
-  },
-}
-
-export const NeutralFilled = {
-  args: {
-    children: 'Published',
-    color: 'neutral',
-    type: 'filled',
-  },
-}
-
-export const RedFilled = {
-  args: {
-    children: 'Published',
-    color: 'red',
-    type: 'filled',
-  },
-}
-
-export const IndigoFilled = {
-  args: {
-    children: 'Published',
-    color: 'indigo',
-    type: 'filled',
-  },
-}
-
-export const GreenFilled = {
-  args: {
-    children: 'Published',
-    color: 'green',
-    type: 'filled',
-  },
-}
-
-export const AmberFilled = {
-  args: {
-    children: 'Published',
-    color: 'amber',
-    type: 'filled',
-  },
-}
-
-export const NeutralSubtle = {
-  args: {
-    children: 'Published',
-    color: 'neutral',
-    type: 'subtle',
-  },
-}
-
-export const RedSubtle = {
-  args: {
-    children: 'Published',
-    color: 'red',
-    type: 'subtle',
-  },
-}
-
-export const IndigoSubtle = {
-  args: {
-    children: 'Published',
-    color: 'indigo',
-    type: 'subtle',
-  },
-}
-
-export const GreenSubtle = {
-  args: {
-    children: 'Published',
-    color: 'green',
-    type: 'subtle',
-  },
-}
-
-export const AmberSubtle = {
-  args: {
-    children: 'Published',
-    color: 'amber',
-    type: 'subtle',
-  },
+  render: () => (
+    <div
+      style={{
+        display: 'inline-grid',
+        gap: 24,
+        gridTemplateColumns: 'auto auto auto',
+      }}
+    >
+      {['subtle', 'fill'].map((variant) =>
+        ['neutral', 'red', 'indigo', 'green', 'amber'].map((color) =>
+          [{}, { leadIcon: 'arrow-up' }, { trailIcon: 'arrow-up' }].map(
+            (icon) => (
+              <Badge color={`${color}-${variant}` as any} {...(icon as any)}>
+                Badge
+              </Badge>
+            ),
+          ),
+        ),
+      )}
+    </div>
+  ),
 }
--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -5,82 +5,93 @@ import { Text } from '../Text/index.js'
 
 type BadgeProps = {
   children: string
-  color?: 'neutral' | 'red' | 'green' | 'indigo' | 'amber'
-  type?: 'filled' | 'subtle'
+  color?:
+    | 'neutral-subtle'
+    | 'neutral-fill'
+    | 'red-subtle'
+    | 'red-fill'
+    | 'indigo-subtle'
+    | 'indigo-fill'
+    | 'green-subtle'
+    | 'green-fill'
+    | 'amber-subtle'
+    | 'amber-fill'
   leadIcon?: IconProps['variant']
   trailIcon?: IconProps['variant']
 }
 
 function Badge({
   children,
-  color = 'neutral',
-  type = 'filled',
+  color = 'neutral-subtle',
   leadIcon,
   trailIcon,
 }: BadgeProps) {
+  if (leadIcon && trailIcon) {
+    throw new Error(
+      'Badge only supports either leadIcon or trailIcon, not both at the same time.',
+    )
+  }
+  const hasIcon = leadIcon || trailIcon
+
   return (
     <div
       style={{
         display: 'inline-flex',
         height: 24,
-        padding: '0 6px',
+        padding: hasIcon ? '0 4px' : '0 6px',
+        gap: 1,
         alignItems: 'center',
         justifyContent: 'center',
         borderRadius: radius[4],
-        ...(type === 'filled' && {
-          ...(color === 'neutral' && {
-            background: colors.neutral100,
-            color: colors.neutralInverted100,
-          }),
-          ...(color === 'red' && {
-            background: colors.red100,
-            color: colors.white100,
-          }),
-          ...(color === 'green' && {
-            background: colors.green100,
-            color: colors.white100,
-          }),
-          ...(color === 'indigo' && {
-            background: colors.indigo100,
-            color: colors.white100,
-          }),
-          ...(color === 'amber' && {
-            background: colors.amber100,
-            color: colors.white100,
-          }),
+        ...(color === 'neutral-fill' && {
+          background: colors.neutral100,
+          color: colors.neutralInverted100,
         }),
-        ...(type === 'subtle' && {
-          ...(color === 'neutral' && {
-            background: colors.neutral20,
-            color: colors.neutral100,
-          }),
-          ...(color === 'red' && {
-            background: colors.red20,
-            color: colors.red100,
-          }),
-          ...(color === 'green' && {
-            background: colors.green20,
-            color: colors.green100,
-          }),
-          ...(color === 'indigo' && {
-            background: colors.indigo20,
-            color: colors.indigo100,
-          }),
-          ...(color === 'amber' && {
-            background: colors.amber20,
-            color: colors.amber100,
-          }),
+        ...(color === 'red-fill' && {
+          background: colors.red100,
+          color: colors.white100,
+        }),
+        ...(color === 'green-fill' && {
+          background: colors.green100,
+          color: colors.white100,
+        }),
+        ...(color === 'indigo-fill' && {
+          background: colors.indigo100,
+          color: colors.white100,
+        }),
+        ...(color === 'amber-fill' && {
+          background: colors.amber100,
+          color: colors.white100,
+        }),
+        ...(color === 'neutral-subtle' && {
+          background: colors.neutral20,
+          color: colors.neutral100,
+        }),
+        ...(color === 'red-subtle' && {
+          background: colors.red20,
+          color: colors.red100,
+        }),
+        ...(color === 'green-subtle' && {
+          background: colors.green20,
+          color: colors.green100,
+        }),
+        ...(color === 'indigo-subtle' && {
+          background: colors.indigo20,
+          color: colors.indigo100,
+        }),
+        ...(color === 'amber-subtle' && {
+          background: colors.amber20,
+          color: colors.amber100,
         }),
       }}
     >
       {leadIcon && <Icon size="small" variant={leadIcon} />}
-      {trailIcon && !leadIcon && <div style={{ width: 2, height: '100%' }} />}
+      {trailIcon && !leadIcon && <div style={{ width: 1, height: '100%' }} />}
       <Text color="inherit" variant="subtext-medium">
         {children}
       </Text>
       {trailIcon && <Icon size="small" variant={trailIcon} />}
-
-      {leadIcon && !trailIcon && <div style={{ width: 2, height: '100%' }} />}
+      {leadIcon && !trailIcon && <div style={{ width: 1, height: '100%' }} />}
     </div>
   )
 }
