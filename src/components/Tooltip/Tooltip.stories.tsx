b2bd2d423d7dfb55ff3968fb611cdf5a9ea71ce5
Vass Bence <49574140+vassbence@users.noreply.github.com>
2024-08-02 15:42:59 +0200

    fix

--- src/components/Tooltip/Tooltip.stories.tsx
+++ src/components/Tooltip/Tooltip.stories.tsx
@@ -10,23 +10,16 @@ export default {
 
 export const OnButton = () => {
   return (
-    <div style={{ display: 'flex', gap: 12, paddingTop: 24 }}>
-      <Tooltip value="Send message" keyHint="Enter">
-        <Button>Send message</Button>
-      </Tooltip>
-      <Tooltip value="Change start date">
-        <Button variant="secondary">July 2, 2024</Button>
-      </Tooltip>
-    </div>
+    <Tooltip value="Send message" keyHint="Enter">
+      <Button>Send message</Button>
+    </Tooltip>
   )
 }
 
 export const OnIconButton = () => {
   return (
-    <div style={{ display: 'flex', paddingTop: 24 }}>
-      <Tooltip value="Close" keyHint="Esc">
-        <IconButton variant="close" />
-      </Tooltip>
-    </div>
+    <Tooltip value="Close" keyHint="Esc">
+      <IconButton variant="close" />
+    </Tooltip>
   )
 }
