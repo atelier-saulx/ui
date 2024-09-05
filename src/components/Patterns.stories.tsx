ec85bb128f6f656743524a93c20ee7dd9cc48dd6
Vass Bence <49574140+vassbence@users.noreply.github.com>
2024-09-05 15:25:06 +0200

    work

--- src/components/Patterns.stories.tsx
+++ src/components/Patterns.stories.tsx
@@ -1,14 +1,5 @@
 import React, { useState } from 'react'
-import {
-  useForm,
-  Button,
-  Modal,
-  TextInput,
-  CheckboxInput,
-  TextAreaInput,
-  FormField,
-  FormFieldGroup,
-} from '../index.js'
+import { Button, Modal, Form, FormFields } from '../index.js'
 
 export default {
   title: 'Patterns (WIP)',
@@ -19,41 +10,12 @@ export default {
 
 export function FormInModalWithDataFromAPI() {
   const [open, setOpen] = useState(false)
-  const form = useForm({
-    validate: async (values) => {
-      const errors = {}
-
-      await new Promise((resolve) => {
-        setTimeout(() => {
-          resolve('foo')
-        }, 2000)
-      })
-
-      if (!values['title']) {
-        errors['title'] = 'title is required'
-      }
-
-      return errors
-    },
-    onSubmit: async (values) => {
-      await new Promise((resolve) => {
-        setTimeout(() => {
-          resolve('foo')
-        }, 2000)
-      })
-
-      console.log('form submitted with', JSON.parse(JSON.stringify(values)))
-
-      setOpen(false)
-    },
-  })
 
   return (
     <>
       <Button
         leadIcon="add"
         onClick={() => {
-          form.resetForm()
           setOpen(true)
         }}
       >
@@ -65,57 +27,67 @@ export function FormInModalWithDataFromAPI() {
         <Modal.Description>
           Add a beautiful little todo here to make sure you remember
         </Modal.Description>
-        <Modal.Body>
-          <FormFieldGroup>
-            <FormField label="Email" error={form.errors['title']}>
-              <TextInput
-                disabled={form.isSubmitting}
-                value={form.values['title'] as string}
-                onChange={(v) => {
-                  form.setValue('title', v)
-                }}
-                error={!!form.errors['title']}
-              />
-            </FormField>
-            <FormField label="Description" error={form.errors['description']}>
-              <TextAreaInput
-                disabled={form.isSubmitting}
-                value={form.values['description'] as string}
-                onChange={(v) => {
-                  form.setValue('description', v)
-                }}
-                error={!!form.errors['description']}
-              />
-            </FormField>
-            <FormField label="Done" error={form.errors['done']}>
-              <CheckboxInput
-                disabled={form.isSubmitting}
-                value={form.values['done'] as boolean}
-                onChange={(v) => {
-                  form.setValue('done', v)
-                }}
-                error={!!form.errors['done']}
-              />
-            </FormField>
-          </FormFieldGroup>
-        </Modal.Body>
-        <Modal.Actions>
-          {({ close }) => (
+        <Form
+          fields={{
+            title: { type: 'text', label: 'Title' },
+            description: { type: 'textarea', label: 'Description' },
+            done: { type: 'checkbox', label: 'Done' },
+          }}
+          validate={async (values) => {
+            const errors = {}
+
+            await new Promise((resolve) => {
+              setTimeout(() => {
+                resolve('foo')
+              }, 2000)
+            })
+
+            if (!values['title']) {
+              errors['title'] = 'title is required'
+            }
+
+            return errors
+          }}
+          onSubmit={async (values) => {
+            await new Promise((resolve) => {
+              setTimeout(() => {
+                resolve('foo')
+              }, 2000)
+            })
+
+            console.log(
+              'form submitted with',
+              JSON.parse(JSON.stringify(values)),
+            )
+
+            setOpen(false)
+          }}
+        >
+          {({ isSubmitting, isDirty, submitForm }) => (
             <>
-              <Button
-                disabled={form.isSubmitting}
-                onClick={close}
-                variant="border"
-                keyHint="Esc"
-              >
-                Close
-              </Button>
-              <Button onClick={form.submitForm} keyHint="Enter">
-                Create Todo
-              </Button>
+              <Modal.Body>
+                <FormFields />
+              </Modal.Body>
+              <Modal.Actions>
+                {({ close }) => (
+                  <>
+                    <Button onClick={close} variant="border" keyHint="Esc">
+                      Close
+                    </Button>
+                    <Button
+                      keyHint="Enter"
+                      disabled={isSubmitting || !isDirty}
+                      loading={isSubmitting}
+                      onClick={submitForm}
+                    >
+                      Create Todo
+                    </Button>
+                  </>
+                )}
+              </Modal.Actions>
             </>
           )}
-        </Modal.Actions>
+        </Form>
       </Modal>
     </>
   )
